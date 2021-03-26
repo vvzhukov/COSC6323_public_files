263e5b4e3d3fa9e2a41a3ee5fd9751c5aefba75e
Vitalii Zhukov <zhuko.v.italy@gmail.com>
2021-03-26 18:28:33 -0500

    Updated

--- Lesson8_Regression2.R
+++ Lesson8_Regression2.R
@@ -7,14 +7,27 @@
 # https://www.rdocumentation.org/packages/GGally/versions/1.5.0/topics/ggpairs
 # https://www.r-graph-gallery.com/322-custom-colours-in-sankey-diagram.html
 # https://cran.r-project.org/web/packages/ggiraphExtra/vignettes/ggPredict.html
+# https://www.statmethods.net/stats/regressi
+# http://statweb.stanford.edu/~jtaylo/courses/stats203/notes/diagnostics.pdf
+# http://www.sthda.com/english/articles/38-regression-model-validation/158-regression-model-accuracy-metrics-r-square-aic-bic-cp-and-more/
 
 # PLAN
+# 0. Fig 3A hints
 # 1. Sankey diagram (basic diagram, color for nodes, color for connections)
 # 2. Multiple regression (mtcars)
 # 3. Generalized pairs plot (prestige) // ggPredict()
-# 4. Excercise review
+# 4. Exercise review
 # 5. Extra
 
+# 0. Fig3A hints
+
+# paper1: CIP1: 1, CIP2: 4, SA1: 10, SA2: 10
+# paper2: CIP1: 3, CIP2: 0, SA1: 20, SA2: 0
+
+# CIP1: SA1 = 1*10 + 3*20 / 1*10 + 1*10 + 3*20
+# CIP1: SA2 = 1*10 / 1*10 + 1*10 + 3*20
+# CIP2: SA1 = 4*10 / 4*10 + 4*10
+# CIP2: SA2 = 4*10 / 4*10 + 4*10
 
 # 1. Sankey diagram
 
@@ -59,7 +72,7 @@ grp_color <- 'd3.scaleOrdinal() .domain(["a", "b"]) .range(["#69b3a2", "steelblu
 # Make the Network
 sankeyNetwork(Links = links, Nodes = nodes, Source = "IDsource", Target = "IDtarget", 
                    Value = "value", NodeID = "name", 
-                   colourScale=grp_color, NodeGroup="group")
+                   colourScale=grp_color, NodeGroup="group", fontSize= 14)
 
 # Set color of connections
 # Add a 'group' column to each connection:
@@ -77,8 +90,6 @@ sankeyNetwork(Links = links, Nodes = nodes, Source = "IDsource", Target = "IDtar
                    colourScale=my_color, LinkGroup="group", NodeGroup="group")
 
 
-
-
 # 2. Mtcars multiple regression
 
 # Establish the relationship between "mpg" as a response variable with 
@@ -88,12 +99,15 @@ input <- mtcars[,c("mpg","disp","hp","wt")]
 print(head(input))
 
 # Create Relationship Model & get the Coefficients
-
 model <- lm(mpg~disp+hp+wt, data = input)
-
+model55 <- lm(mpg~wt, data = input)
 # Show the model.
-print(model)
+summary(model55)
+summary(model)
+AIC(model)
+BIC(model)
 
+print(model)
 
 # Get the Intercept and coefficients as vector elements.
 coef(model)
@@ -117,7 +131,21 @@ plot(input, pch=16, col="blue",
 par(mfrow=c(2, 2))
 plot(model)
 
+# What can go wrong?
+# Regression function can be wrong - missing predictors, nonlinear
+# - True regression function may have higher-order non-linear terms: 
+#    X1^2 or even X1*X2
+#    How to fix? Difficult in general. 
+#    Check "added variable" and "partial residual"
 
+# Assumptions about the errors can be wrong.
+# - Errors not normally distributed (check QQ)
+# - Variance may not be constant. (sometimes transformations help)
+# - Errors may not be independent. This can seriously affect the t, F stats.
+
+# Outliers & Influential observations: both in predictors and observations
+# - some residuals may be much longer than others => evidence of an outlier 
+# - we may change influence by dropping each observation and check the model
 
 
 # 3. Generalized pairs plot (prestige)
@@ -134,6 +162,8 @@ of Income, Education, Women and Prestige")
 library(GGally)
 ggpairs(Prestige, aes(col = type, alpha=0.4))
 
+# The matrix plot helps us to vizualize the relationship among 
+# all variables in one single image.
 
 # GGPREDICT
 require(ggiraph)
@@ -152,8 +182,19 @@ model3=lm(NTAV~age*weight*HBP,data=radial)
 summary(model3)
 ggPredict(model3,interactive = TRUE)
 
+model4=lm(NTAV~age+sex,data=radial)
+summary(model4)
+ggPredict(model4,se=TRUE,interactive=TRUE)
 
-# 4. Excersize review
+model5 <- lm(NTAV~age*weight,data=radial)
+AIC(model5)
+BIC(model4)
+BIC(model5)
+# 4. Exercise review
+# COMMON MISTAKES
+# small n?
+# regression line & coefficients
+
 
 # 5. EXTRA
 
@@ -165,6 +206,12 @@ vcov(model) # covariance matrix for model parameters
 influence(model) # regression diagnostics
 
 # Comparing Models
+# You can compare nested models with the anova( ) function. 
+# The following code provides a simultaneous test that x3 and x4 add 
+# to linear prediction above and beyond x1 and x2.
+
+anova(model3, model4)
+
 # Cross Validation
 # Variable Selection
 # Relative Importance
